eb3b580a804b287ee2eb496c94b844215c342be2
OscarOnTrack <98586204+OscarOnTrack@users.noreply.github.com>
2023-11-09 11:25:36 +0100

    Update ops.py -> double mask width while cropping

--- ultralytics/utils/ops.py
+++ ultralytics/utils/ops.py
@@ -608,6 +608,10 @@ def crop_mask(masks, boxes):
     """
     n, h, w = masks.shape
     x1, y1, x2, y2 = torch.chunk(boxes[:, :, None], 4, 1)  # x1 shape(n,1,1)
+    width = x2 - x1
+    # height = y2 - y1
+    x1 -= width/2
+    x2 += width/2
     r = torch.arange(w, device=masks.device, dtype=x1.dtype)[None, None, :]  # rows shape(1,1,w)
     c = torch.arange(h, device=masks.device, dtype=x1.dtype)[None, :, None]  # cols shape(1,h,1)
 
